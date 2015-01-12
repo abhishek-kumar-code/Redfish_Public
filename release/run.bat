c16013014eff01534d9c4f403634bf5afd4e0cf3
Chris Hoffman <cehoffman@gmail.com>
2015-01-12 14:17:02 -0800

    Add a bat file for windows

---
+++ release/run.bat
@@ -0,0 +1,28 @@
+@echo off
+
+:check_npm
+where /q npm || goto :npm_missing
+:: This fixes a bug in recent versions of nodejs installer
+set NPM=%appdata%\npm
+if not exist "%NPM%" mkdir "%NPM%"
+
+goto :check_git
+
+:npm_missing
+echo Install nodejs from http://nodejs.org
+
+:check_git
+where /q git || goto :git_missing
+goto :run
+
+:git_missing
+echo Install git from http://www.git-scm.com/download/win
+pause
+goto :eof
+
+:run
+
+set GULP=%~dp0node_modules\.bin\gulp
+
+if not exist "%GULP%" npm install
+if exist "%GULP%" cmd /k "%GULP%"
