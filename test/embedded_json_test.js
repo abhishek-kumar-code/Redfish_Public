cdb8eda1898ace61b9b1119cce0fb5faeef559f4
Chris Hoffman <Christopher.Hoffman@Emerson.com>
2015-08-04 14:23:43 -0500

    Move regex all machtes to util for test

--- test/embedded_json_test.js
+++ test/embedded_json_test.js
@@ -4,24 +4,8 @@ var join = require('path').join;
 var jsonlint = require('jsonlint');
 var fs = require('fs');
 var assert = require('assert');
+var execAll = require('./utils').execAll;
 
-function execAll(regex, str) {
-  var match = null;
-  var matches = [];
-
-   while (match = regex.exec(str)) {
-     var matchArray = [];
-     for (var i in match) {
-       if (parseInt(i) == i) {
-         matchArray.push(match[i].trim());
-       }
-     }
-     matchArray.lineNumber = str.substr(0, match.index).split("\n").length + 1;
-     matches.push(matchArray);
-   }
-
-   return matches.length > 0 ? matches : null;
-}
 
 var files = glob.sync('*.md');
 
