d1f0f6d916f950a2b03c49b64bbe4c4c32523175
Chris Hoffman <Christopher.Hoffman@Emerson.com>
2015-08-05 11:03:32 -0500

    Make the mockup tests run over each mockup under mockups

--- test/mockup-syntax-test.js
+++ test/mockup-syntax-test.js
@@ -8,8 +8,7 @@ var traverse = require('traverse');
 var url = require('url');
 var jptr = require('json8-pointer');
 
-var files = glob.sync(path.join('mockups', 'development', '**', 'index.json'));
-var syntaxSuite = {}, consistencySuite = {};
+var mockupSuite = vows.describe('Mockups');
 
 var LinkProperties = [
   /^@odata\.id$/,
@@ -23,68 +22,73 @@ function isLink(key) {
   }
 }
 
-var linkToFile = {}
 
-files.forEach(function(file) {
-  var decomp = path.parse(file);
+glob.sync(path.join('mockups', '*/')).forEach(function(mockup) {
+  var files = glob.sync(path.join(mockup, '**', 'index.json'));
+  var syntaxBatch = {}, consistencyBatch = {};
+  var linkToFile = {}
+  var fileToJSON = {};
 
-  // Get everything after mockups/<type> in path
-  var link = decomp.dir.split(path.sep).slice(2)
+  files.forEach(function(file) {
+    var decomp = path.parse(file);
 
-  // Prepend with /redfish/v1
-  link.unshift('', 'redfish', 'v1', '');
+    // Get everything after mockups/<type> in path
+    var link = decomp.dir.split(path.sep).slice(2)
 
-  linkToFile[path.normalize(link.join('/'))] = file;
-})
+    // Prepend with /redfish/v1
+    link.unshift('', 'redfish', 'v1', '');
 
-var fileToJSON = {};
+    linkToFile[path.normalize(link.join('/'))] = file;
 
-files.forEach(function(file) {
-  syntaxSuite[file] = {
-    topic: function() {
-      var self = this;
+    syntaxBatch[file] = {
+      topic: function() {
+        var self = this;
 
-      fs.readFile(file, 'utf-8', function(err, txt) {
-        try {
-          var json = jsonlint.parse(txt);
-          fileToJSON[file] = json;
-          self.callback(null, json);
-        } catch(e) {
-          self.callback('Failed to parse\n' + e.message);
-        }
-      });
-    },
-    'is valid JSON': function(err, json) {
-      assert.isNull(err);
-    }
-  }
-
-  consistencySuite[file] = {
-    'links are internally consistent': function() {
-      var json = fileToJSON[file];
-      if (!json) return;
-
-      var walker = traverse(json);
-
-      walker.forEach(function() {
-        if (!this.isLeaf) return;
-
-        if (isLink(this.key)) {
-          var link = url.parse(this.node);
-          var refd = fileToJSON[linkToFile[link.pathname]];
-
-          assert(refd !== undefined, 'invalid link at path /' + this.path.join('/') + ' with content ' + this.node + '. No such file exists at path.');
-          if (link.hash) {
-            refd = jptr.find(refd, link.hash.slice(1));
-            assert(refd !== undefined, 'invalid fragment component at path /' + this.path.join('/') + ' with content ' + link.hash);
+        fs.readFile(file, 'utf-8', function(err, txt) {
+          try {
+            var json = jsonlint.parse(txt);
+            fileToJSON[file] = json;
+            self.callback(null, json);
+          } catch(e) {
+            self.callback('Failed to parse\n' + e.message);
           }
-        }
-      });
+        });
+      },
+      'is valid JSON': function(err, json) {
+        assert.isNull(err);
+      }
     }
-  }
-})
 
-vows.describe('Development Mockup')
-  .addBatch(syntaxSuite)
-  .addBatch(consistencySuite)
-  .export(module);
+    consistencyBatch[file] = {
+      'links are internally consistent': function() {
+        var json = fileToJSON[file];
+        if (!json) return;
+
+        var walker = traverse(json);
+
+        walker.forEach(function() {
+          if (!this.isLeaf) return;
+
+          if (isLink(this.key)) {
+            var link = url.parse(this.node);
+            var refd = fileToJSON[linkToFile[link.pathname]];
+
+            assert(refd !== undefined, 'invalid link at path /' + this.path.join('/') + ' with content ' + this.node + '. No such file exists at path.');
+            if (link.hash) {
+              refd = jptr.find(refd, link.hash.slice(1));
+              assert(refd !== undefined, 'invalid fragment component at path /' + this.path.join('/') + ' with content ' + link.hash);
+            }
+          }
+        });
+      }
+    }
+  })
+
+  // vows.describe(mockup.replace(/^[a-z]/i, function(x) { return x.toUpperCase() }) + ' Mockup')
+  mockupSuite
+    .addBatch(syntaxBatch)
+    .addBatch(consistencyBatch)
+    // .export(module);
+});
+
+mockupSuite.export(module);
