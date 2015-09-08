208543969894d8b7574d7340a4cd701030d262c1
Chris Hoffman <Christopher.Hoffman@Emerson.com>
2015-09-08 20:53:26 +0200

    Fix #789, an off by one on sending JSON data

--- mockups/development/server.js
+++ mockups/development/server.js
@@ -37,7 +37,7 @@ var head = function(res, status, body, overrides) {
   }
 
   if (body) {
-    headers['Content-Length'] = body.length ? body.length : body;
+    headers['Content-Length'] = body.length ? body.length + 1: body;
   }
 
   res.writeHead(status, headers);
