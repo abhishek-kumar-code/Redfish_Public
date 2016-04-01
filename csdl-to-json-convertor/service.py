bd47b07adba4016d99a298d1547a09a5a6df799e
Michael Pizzo <mikepizzo@hotmail.com>
2016-04-01 12:44:12 -0700

    Fix reference to redfish schema

--- csdl-to-json-convertor/service.py
+++ csdl-to-json-convertor/service.py
@@ -43,7 +43,7 @@ if enable_debugging == True:
 schemaLocation = "http://redfish.dmtf.org/schemas/" 
 schemaBaseLocation = schemaLocation + "v1/"
 odataSchema = schemaBaseLocation + "odata.4.0.0.json"
-redfishSchema = schemaLocation + "v1/redfish-schema.v1_0_0.json"
+redfishSchema = schemaLocation + "v1/redfish-schema.v1_1_0.json"
 
 #########################################################################################################
 # Class Name: JsonSchemaGenerator                                                                       #
