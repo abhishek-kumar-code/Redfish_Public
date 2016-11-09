c37c68ebe5a3f6a41cab11a0f784602799725230
Patrick Boyd <Patrick_Boyd@Dell.com>
2016-11-09 21:48:14 +0000

    Add test for Complex Types with Permissions

--- test/metadata-syntax-test.js
+++ test/metadata-syntax-test.js
@@ -14,7 +14,7 @@ let ucumError = false;
 const unitsWhiteList = ['RPM'];
 //This does not contain a full CSDL parser. This whitelist lists complex types that are utilized across files
 const complexTypeWhitelist = ['Resource.Status', 'Resource.Oem', 'Resource.v1_1_0.Location',
-                              'Storage.v1_0_0.Protocol', 'VLanNetworkInterface.v1_0_0.VLAN'];
+                              'Resource.v1_1_0.Identifier', 'VLanNetworkInterface.v1_0_0.VLAN'];
 
 function getUcumXML(callback, context, end)
 {
@@ -132,13 +132,13 @@ files.forEach(function(file) {
           {
               var propName = property.attr('Name').value();
               var propType = property.attr('Type').value(); 
-              if(complexTypeWhitelist.indexOf(propType) !== -1)
-              {
-                continue;
-              }
               if(propType.startsWith('Collection('))
               {
                 propType = propType.substring(11, propType.length-1);
+              }
+              if(complexTypeWhitelist.indexOf(propType) !== -1)
+              {
+                continue;
               }
               var propArray = propType.split('.');
               var elemName = propArray[propArray.length - 1];
@@ -148,6 +148,45 @@ files.forEach(function(file) {
                 throw new Error('Property '+propName+' of '+propType+' lacks permission!');
               }
           }
+      }
+    },
+    'complex types should not have permissions':  function(err, txt) {
+      if(this.context.name.includes('RedfishExtensions_v1.xml'))
+      {
+        //Ignore the RedfishExtensions file. It's just about annotations
+        return;
+      }
+      let doc = xmljs.parseXml(txt);
+      let properties = doc.find('//*[local-name()="Property"]');
+      if(properties.length === 0)
+      {
+        //No properties are perfectly acceptable
+        return;
+      }
+      for(let i = 0; i < properties.length; i++)
+      {
+        var property = properties[i];
+        var permissions = property.find('*[local-name()="Annotation"][@Term="OData.Permissions"]');
+        if(permissions.length !== 0)
+        {
+          var propName = property.attr('Name').value();
+          var propType = property.attr('Type').value();
+          if(propType.startsWith('Collection('))
+          {
+            propType = propType.substring(11, propType.length-1);
+          }
+          if(complexTypeWhitelist.indexOf(propType) !== -1)
+          {
+            throw new Error('Property '+propName+' of '+propType+' has permission!');
+          }
+          var propArray = propType.split('.');
+          var elemName = propArray[propArray.length - 1];
+          let complexTypes = doc.find('//*[local-name()="ComplexType"][@Name="'+elemName+'"]');
+          if(complexTypes.length !== 0)
+          {
+            throw new Error('Property '+propName+' of '+propType+' has permission!');
+          }
+        }
       }
     },
     'no empty Schema tags': function(err, txt) {
