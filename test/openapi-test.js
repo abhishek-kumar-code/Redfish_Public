869ddd1eb1cd6897995c07ba2c8b277af7375ddf
Patrick Boyd <Patrick_Boyd@Dell.com>
2018-09-21 19:29:09 +0000

    Switch to swagger-parser for OpenAPi validation

--- test/openapi-test.js
+++ test/openapi-test.js
@@ -5,7 +5,7 @@ var yamllint = require('yaml-lint');
 var path = require('path');
 var fs = require('fs');
 var yaml = require('js-yaml');
-var openapival = require('openapi-schema-validator');
+var SwaggerParser = require('swagger-parser');
 
 var yamlSuite = vows.describe('YAML');
 
@@ -20,11 +20,9 @@ function isValidYAML(err, txt)
 function isValidOpenAPI(err, txt)
 {
     let doc = yaml.safeLoad(txt);
-    let validator = new openapival();
-    let res = validator.validate(doc, 3);
-    if(res && res.errors !== undefined && res.errors.length > 0) {
-      throw Error('Invalid Open API!\n' + JSON.stringify(res.errors));
-    }
+    SwaggerParser.validate(doc).then().catch(function(err){
+      throw err;
+    });
 }
 
 function testOpenAPISchema(schema)
@@ -33,7 +31,7 @@ function testOpenAPISchema(schema)
     validBatch[schema] = {
       topic: function() {fs.readFile(schema, this.callback);}, 
       'is valid YAML': isValidYAML,
-      //'is valid OpenAPI': isValidOpenAPI
+      'is valid OpenAPI': isValidOpenAPI
     };
     yamlSuite.addBatch(validBatch);
 }
